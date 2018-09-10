75ca25bf3cd93aa699d82497b0c70b72e03e2579
merciofr <mercio.andrade@ufrpe.br>
2018-09-10 01:06:32 -0300

    esboço de leitura de dados do firebase

--- listagem-livros/listagem-livros.component.js
+++ listagem-livros/listagem-livros.component.js
@@ -2,10 +2,71 @@ angular.module('listagemLivros').component('listagemLivros', {
 
   templateUrl: 'listagem-livros/listagem-livros.template.html',
 
-  controller: function ListagemLivrosController() {
+  controller: function ListagemLivrosController($scope) {
+
+    this.teste = 'teste';
+
+    $scope.phones = [
+        {
+            name: 'Nexus S',
+            snippet: 'Fast just got faster with Nexus S.',
+            age: '1'
+          }, {
+            name: 'Motorola XOOM™ with Wi-Fi',
+            snippet: 'The Next, Next Generation tablet.',
+            age: '2'
+          }, {
+            name: 'MOTOROLA XOOM™',
+            snippet: 'The Next, Next Generation tablet.',
+            age: '3'
+          }
+    ];
+
+    this.arrayLivros = [];
+
+
+
+    // $scope.arrayLivros.push({
+    //   titulo: 'Sistemas Distribuidos',
+    //   autor: 'Tanenbaum',
+    //   preco: '50'
+    // });
+
+
 
     firebase.database().ref('livros').on('value', function snapshot(s) {
-        window.console.log(s.val());
+
+        let arrayLivros = [];
+        let livros = s.val();
+        
+        for(let key in livros) {
+          let obj = {};
+          obj.autor = livros[key].autor;
+          obj.titulo = livros[key].titulo;
+          obj.preco = livros[key].preco;
+          arrayLivros.push(obj);
+
+       }
+
+
+       this.arrayLivros = arrayLivros.slice();
+
+       console.log(arrayLivros.slice());
+
+       console.log(this.arrayLivros);
+
     });
+
+    // //este código é executado antes que os dados tenham sido recebidos do servidor
+    // this.arrayLivros = arrayLivros.slice();
+    //
+    // console.log(arrayLivros.slice());
+    //
+    // console.log(this.arrayLivros);
+
+    //console.log($scope.phones);
+    //this._arrayLivros = $scope.arrayLivros.slice();
+
+
   }
 });
