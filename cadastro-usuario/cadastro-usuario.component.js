75813ff33504465f93a021fd02c2c4b57997b744
merciof <mercio.andrade@ufrpe.br>
2018-11-05 19:42:13 -0300

    correção de erro em nomes de arquivos e adição de componente 'cadastro-usuario'

---
+++ cadastro-usuario/cadastro-usuario.component.js
@@ -0,0 +1,7 @@
+angular.module('cadastroLivro').component('cadastroLivro',
+{
+    templateUrl: 'cadastro-livro/cadastro-livro.template.html',
+    controller: function cadastroUsuarioController($scope) {
+
+    }
+});
