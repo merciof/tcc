26c227599c2d551856db11c58c4eeb65e3ee18dc
merciof <mercio.andrade@ufrpe.br>
2018-11-23 18:39:07 -0300

    adição de imports de components no index

--- components/home-jumbotron/home-jumbotron.component.js
+++ components/home-jumbotron/home-jumbotron.component.js
@@ -1,4 +1,4 @@
-angular.module('homeJunbotron').component('homeJumbotron',
+angular.module('homeJumbotron').component('homeJumbotron',
 {
     templateUrl: 'components/home-jumbotron/home-jumbotron.template.html',
     controller: function homeJumbotrontController(){
