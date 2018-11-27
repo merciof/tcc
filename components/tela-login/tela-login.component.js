b987140cd0232c074ab1749d1d77d84381c28085
merciof <mercio.andrade@ufrpe.br>
2018-11-26 23:06:46 -0300

    adição do component tela-login

---
+++ components/tela-login/tela-login.component.js
@@ -0,0 +1,6 @@
+angular.module('telaLogin').component('telaLogin',{
+    templateUrl: 'components/tela-login/tela-login.template.html',
+    controller: function TelaLoginController() {
+
+    }
+});
--- livrosApp.module.js
+++ livrosApp.module.js
@@ -2,14 +2,15 @@ angular.module('livrosApp',['headerHome',
 'footerHome',
 'cardLivro',
 'jumbotronHome',
+'telaLogin',
 'ngRoute'])
 .config(['$routeProvider', function config($routeProvider) {
     $routeProvider
-    .when('/', {
-      template: ''
+    .when('/login', {
+      template: '<tela-login></tela-login>'
     })
     .when('/home', {
       template: '<header-home></header-home>' + '<main role="main"><jumbotron-home></jumbotron-home>' 
                 + '<card-livro></card-livro></main>'  + '<footer-home></footer-home>'
-    }).otherwise('/')
+    }).otherwise('/login')
   }]);
