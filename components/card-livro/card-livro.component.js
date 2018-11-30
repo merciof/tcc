9c9dd5b9232a5577978fc6fff00f2cdb97ef87e3
merciof <mercio.andrade@ufrpe.br>
2018-11-30 00:08:38 -0300

    adição de elementos no $scope do component card-livro

--- components/card-livro/card-livro.component.js
+++ components/card-livro/card-livro.component.js
@@ -1,6 +1,34 @@
-angular.module('cardLivro').component('cardLivro',{
+angular.module('cardLivro').component('cardLivro', {
     templateUrl: 'components/card-livro/card-livro.template.html',
-    controller: function CardLivroController() {
+    controller: function CardLivroController($scope) {
+        $scope.livros = [{
 
+            "nome": "Java Como Programar",
+            "edicao": "sexta",
+            "sinopse": "blablabla",
+            "autor": "Deitel",
+            "estado": "semi novo"
+        }, {
+
+            "nome": "Java Como Programar 2",
+            "edicao": "sexta",
+            "sinopse": "blablabla",
+            "autor": "Deitel",
+            "estado": "semi novo"
+        }, {
+
+            "nome": "Java Como Programar 3",
+            "edicao": "sexta",
+            "sinopse": "blablabla",
+            "autor": "Deitel",
+            "estado": "semi novo"
+        }, {
+
+            "nome": "Java Como Programar 4",
+            "edicao": "sexta",
+            "sinopse": "blablabla",
+            "autor": "Deitel",
+            "estado": "semi novo"
+        }];
     }
 });
