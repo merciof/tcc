7e3191d4607b20a8a010cddc98a1c7e33c22a0f5
merciof <mercio.andrade@ufrpe.br>
2018-12-08 22:47:58 -0300

    criação dos components cadastro-anuncio e cadastro-usuario

---
+++ components/cadastro-anuncio/cadastro-anuncio.component.js
@@ -0,0 +1,6 @@
+angular.module('cadastroAnuncio').component('cadastroAluno', {
+    templateUrl: 'components/cadastro-anuncio/cadastro-anuncio.template.html',
+    controller: function CadastroAnuncioController() {
+
+    }
+});
---
+++ components/cadastro-usuario/cadastro-usuario.component.js
@@ -0,0 +1,6 @@
+angular.module('cadastroUsuario').component('cadastroUsuario', {
+    templateUrl: 'components/cadastro-usuario/cadastro-usuario.template.html',
+    controller: function CadastroUsuarioController() {
+
+    }
+});
