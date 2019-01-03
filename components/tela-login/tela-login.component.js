c1577afa4be8e8e35c344210971a6c380910956d
Mércio Filho <merciofilho@gmail.com>
2019-01-03 22:18:42 +0000

    adição de template do componte 'cadastro-anuncio', responsável pelo front-end da criação de anúncios

--- components/tela-login/tela-login.component.js
+++ components/tela-login/tela-login.component.js
@@ -2,16 +2,16 @@ angular.module('telaLogin').component('telaLogin',{
     templateUrl: 'components/tela-login/tela-login.template.html',
     controller: function TelaLoginController($scope, $location) {
          
-         $scope.goHome = function goHome() {
+        //  $scope.goHome = function goHome() {
              
-            $location.path('/home'); 
+        //     $location.path('/home'); 
             
-        } 
+        // } 
         
         $scope.goCadastro = function goCadastro() {
             
-            
-            $location.path('/cadastro');
+            alert('Executando função goCadastro!');
+            $location.path('/anuncio');
         
         }
     }
--- livrosApp.routes.js
+++ livrosApp.routes.js
@@ -1,17 +1,26 @@
 angular.module('livrosApp')
 .config(['$routeProvider', '$locationProvider', function config($routeProvider, $locationProvider) {
     $routeProvider
+    
     .when('/login', {
       name: 'login',
       template: '<tela-login></tela-login>'
     })
-    .when('/home', {
-      name: 'home',
-      template: '<header-home></header-home>' + '<main role="main"><jumbotron-home></jumbotron-home>' 
-                + '<card-livro></card-livro></main>'  + '<footer-home></footer-home>'
-    }).when('/cadastro', {
-      name: 'cadastro',
+    
+    // .when('/home', {
+    //   name: 'home',
+    //   template: '<h1>Home Template</h1>' 
+    // })
+    
+    .when('/cadastro-usuario', {
+      name: 'cadastro-usuario',
       template: '<cadastro-usuario></cadastro-usuario>'
     })
+    
+    .when('/anuncio', {
+      name: 'anuncio',
+      template: '<cadastro-anuncio></cadastro-anuncio>'
+    })
+    
     .otherwise('/login')
   }]);
